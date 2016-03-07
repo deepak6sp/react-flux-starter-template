a8a88e42525133fcc9658dc959de70fe2cbce96a
Deepak Prakash <deepak6sp@users.noreply.github.com>
2016-03-07 22:56:53 +1100

    Update Stores.js

--- react_src/stores/Stores.js
+++ react_src/stores/Stores.js
@@ -2,7 +2,7 @@ import MyDispatcher from "../dispatcher/Dispatcher";
 import MyConstants from "../constants/Constants";
 import MyActions from '../actions/Actions';
 var EventEmitter  =  require('events').EventEmitter;
-import assign from 'Object=assign';
+import assign from 'Object-assign';
 import MyApi from '../utils/MyApi';
 
 let CHANGE_EVENT = 'change';
@@ -10,7 +10,7 @@ let CHANGE_EVENT = 'change';
 let movies = new Array();
 let selected = '';
 
-Var MyStore = assign({},EventEmitter.prototype,{
+var MyStore = assign({},EventEmitter.prototype,{
 
 	emitChange :  function(){
 		this.emit(CHANGE_EVENT);
@@ -19,7 +19,7 @@ Var MyStore = assign({},EventEmitter.prototype,{
 		this.on('change',callback);
 	},
 	removeChangeListener: function(callback){
-		this.removeListener('change'callback);
+		this.removeListener('change',callback);
 	}
 
 });
